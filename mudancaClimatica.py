6d9b941b8f7027ef6ae2161e596531af1d84d2cf
Breno Alberto <brenoaps435@gmail.com>
2018-04-27 12:51:49 -0300

    Add Linear Regression

--- mudancaClimatica.py
+++ mudancaClimatica.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression as LinReg
 
 
 plt.style.use('ggplot')
@@ -27,7 +28,23 @@ plt.xlabel("Ano")
 plt.ylabel("Temperatura Terrestre Média")
 plt.show()
 
+# Model || Regressão Linear
+x = grouped.index.values.reshape(-1, 1)
+y = grouped['LandAverageTemperature'].values
 
+reg = LinReg()
+reg.fit(x, y)
+y_pred = reg.predict(x)
+print("Precisão: " + str(reg.score(x, y)))
+
+plt.figure(figsize = (15, 5))
+plt.title("Regressão Linear")
+plt.scatter(x = x, y = y_pred)
+plt.scatter(x = x, y = y, c = "r")
+plt.show()
+
+
+print("Predict para temperatura em 2050: " + str(reg.predict(2050)))
 
 
 
